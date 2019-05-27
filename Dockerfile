85f9c761a22923c22d6b3d2bd5de2386d6182ed6
Einar Persson <me@einarpersson.com>
2019-05-27 17:22:29 +0200

    refactor: New messaging protocol (#97)
    * New protocol implemented for limited, happy path of connect without permissions

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10-alpine
+FROM node:12-alpine
 
 # Setup user
 ENV USER=natreg-user
--- adapters/operator.js
+++ adapters/operator.js
@@ -1,8 +1,9 @@
-const { create, utils } = require('@mydata/client')
+const { create, utils } = require('@egendata/client')
 
 const client = create({
   displayName: 'National registration',
   description: 'This is the national registration of the Kingdom of Sweden',
+  iconURI: `${process.env.CLIENT_ID || 'http://localhost:5000'}/favicon-96x96.png`,
   clientId: process.env.CLIENT_ID || 'http://localhost:5000', // Application domain with protocol
   operator: process.env.OPERATOR_URL || 'http://localhost:3000', // URL of Operator
   clientKeys: {
